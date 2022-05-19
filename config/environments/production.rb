91d747fa72fe41acc1b99df77bf6091e3a18e67c
Babin Bohora <bohorababin1@gmail.com>
2022-05-20 00:44:11 +0900

    one more try to make a fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://redistogo-graceful-09767.herokuapp.com/cable'
+  config.action_cable.url = 'wss://game-room-center.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = ['https://nibab-boo.github.io', /http:\/\/nibab-boo.github.io.*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
